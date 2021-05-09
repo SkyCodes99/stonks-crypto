f15d64c2a74e5c63b7af37621b131e3f5baa4fd4
ConnorSwis <connorswislow@gmail.com>
2021-05-09 17:49:13 -0400

    commit

--- discord/bot.py
+++ discord/bot.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pathlib
 
@@ -6,15 +7,24 @@ import yaml
 import discord
 from discord.ext import commands
 
+
 ABS_PATH = pathlib.Path(__file__).parent.absolute()
 COG_FOLDER = os.path.join(ABS_PATH, 'cogs')
+DEFAULT_PREFIX='$'
 
-def command_prefix(message, client):
+def command_prefix(client: commands.Bot, message: discord.Message):
+    with open(os.path.join(ABS_PATH, 'prefixes.json'), 'r', encoding='utf-8') as f:
+        prefixes = json.load(f)
+    if (prefix:=prefixes.get(str(message.guild.id))):
+        return prefix
+    with open(os.path.join(ABS_PATH, 'prefixes.json'), 'w', encoding='utf-8') as f:
+        prefixes[str(message.guild.id)] = '$'
+        json.dump(prefixes, f)
     return '$'
 
 
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix='command_prefix', owner_ids=[640393413425889314, 516206994718326795], intents=intents)
+client = commands.Bot(command_prefix=command_prefix, owner_ids=[640393413425889314, 516206994718326795], intents=intents)
 
 client.remove_command('help')
 
@@ -24,6 +34,7 @@ for filename in os.listdir(COG_FOLDER):
 
 
 if __name__ == '__main__':
-    with open('./discord/config.yaml') as f:
+    with open(os.path.join(ABS_PATH, 'config.yaml')) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     client.run(config['bot']['token'])
+"febfuibewuifbryuierbfueibfyrueibfeyruifbryeuifbryeifbreuibfyruei"
--- discord/cogs/help_command.py
+++ discord/cogs/help_command.py
@@ -1,9 +1,15 @@
+import json
+import os
+import pathlib
+
 import discord
+import helpers
 from discord.ext import commands
 from discord.ext.commands.core import command
 from discord.ext.commands.errors import *
-import helpers
 
+
+ABS_PATH = pathlib.Path(__file__).parent.parent.absolute()
 
 class Help(commands.Cog):
     def __init__(self, client: commands.Bot):
@@ -11,6 +17,7 @@ class Help(commands.Cog):
 
     @commands.command(brief="Lists commands and gives info.", usage="help *command")
     async def help(self, ctx, request=None):
+        prefix = self.client.command_prefix(self.client, ctx.message)
         if not request:
             embed = helpers.make_embed(title="Commands")
             commands_list = [(name, [command for command in cog.get_commands() if not command.hidden]) for name, cog in self.client.cogs.items()]
@@ -18,7 +25,7 @@ class Help(commands.Cog):
                 if len(cog_commands) != 0:
                     embed.add_field(
                         name=name,
-                        value='\n'.join([f'`{self.client.command_prefix}`{command}' for command in cog_commands]),
+                        value='\n'.join([f'`{prefix}`{command}' for command in cog_commands]),
                         inline=True
                         )
         else:
@@ -27,7 +34,7 @@ class Help(commands.Cog):
                 await ctx.send(f"Command '{request}' doesn't exist")
                 return
             embed = helpers.make_embed(title=com.name, description=com.brief, footer="* optional")                       
-            embed.add_field(name='Usage:', value='`'+self.client.command_prefix+com.usage+'`')
+            embed.add_field(name='Usage:', value='`'+prefix+com.usage+'`')
         await ctx.send(embed=embed)
 
     @commands.command(hidden=True)
@@ -35,6 +42,15 @@ class Help(commands.Cog):
     async def kill(self, ctx: commands.Context):
         self.client.remove_cog('handlers')
         await self.client.logout()
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def prefix(self, ctx: commands.Context, prefix: str):
+        with open(os.path.join(ABS_PATH, 'prefixes.json'), 'r', encoding='utf-8') as f:
+            prefixes = json.load(f)
+        prefixes[str(ctx.guild.id)] = prefix
+        with open(os.path.join(ABS_PATH, 'prefixes.json'), 'w', encoding='utf-8') as f:
+            json.dump(prefixes, f)
 
 
 def setup(client):
